fae58588a967386fc3b67944de0ad21588aab761
Marcin Kaczor <marcinkwa2@gmail.com>
2024-12-16 12:16:54 +0100

    Moved generator function out of main

--- generator.py
+++ generator.py
@@ -1,14 +1,11 @@
 import numpy as np
  
 n = 10000  # Number of matrices to generate
-
-def main():
-    def generate_matrix(size, max_value=10000.0):
+def generate_matrix(size, max_value=10000.0):
         matrix = np.random.uniform(1, max_value, size=(size, size))  # Random float values
         np.fill_diagonal(matrix, 0.0)  # Set diagonal to 0
         return matrix
-
-
+def main():
     for i in range(5, n):
         matrix = generate_matrix(i)
         np.savetxt(f"data/matrix_{i}.txt", matrix, fmt='%.1f', header=f"{i}", comments='')
